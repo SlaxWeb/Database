a2343b6b6fe62d2ba9182b99e5bdb76186b5f05d
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-06-09 21:18:53 +0200

    add test for table name setter

--- tests/unit/BaseModelTest.php
+++ tests/unit/BaseModelTest.php
@@ -72,7 +72,7 @@ class BaseModelTest extends \PHPUnit_Framework_TestCase
 
         $this->_inflector = $this->getMockBuilder(Inflector::class)
             ->disableOriginalConstructor()
-            ->setMethods(null)
+            ->setMethods(["pluralize", "camelize", "underscore"])
             ->setMockClassName("InflectorMock")
             ->getMock();
 
@@ -87,14 +87,125 @@ class BaseModelTest extends \PHPUnit_Framework_TestCase
     {
     }
 
-    public function testModelInit()
+    /**
+     * Test Table Name Auto Setter
+     *
+     * Ensure that the table name is set when permitted by config, and not set before,
+     * and the name is pluralized and put into correct form, based on config.
+     *
+     * @return void
+     */
+    public function testTableNameSet()
     {
         $model = $this->getMockBuilder(BaseModel::class)
             ->disableOriginalConstructor()
             ->setMethods(null)
-            ->setMockClassName("TestTable")
+            ->setMockClassName("Test")
             ->getMock();
 
+        $this->_config->expects($this->exactly(22))
+            ->method("offsetGet")
+            ->will(
+                $this->onConsecutiveCalls(
+                    // deny table setting
+                    false,
+
+                    // allow table setting, no pluralization or format manipulation
+                    true,
+                    false,
+                    false,
+
+                    // allow table setting, pluralize, no format manipulation
+                    true,
+                    true,
+                    false,
+
+                    // allow table setting, no pluralization, camel case with ucfirst
+                    true,
+                    false,
+                    BaseModel::TBL_NAME_CAMEL_UCFIRST,
+
+                    // allow table setting, no pluralization, camel case with lcfirst
+                    true,
+                    false,
+                    BaseModel::TBL_NAME_CAMEL_LCFIRST,
+
+                    // allow table setting, no pluralization, underscore
+                    true,
+                    false,
+                    BaseModel::TBL_NAME_UNDERSCORE,
+
+                    // allow table setting, no pluralization, uppercase
+                    true,
+                    false,
+                    BaseModel::TBL_NAME_UPPERCASE,
+
+                    // allow table setting, no pluralization, lowercase
+                    true,
+                    false,
+                    BaseModel::TBL_NAME_LOWERCASE
+                )
+            );
+
+        $this->_inflector->expects($this->once())
+            ->method("pluralize")
+            ->with("Test")
+            ->willReturn("Tests");
+
+        $this->_inflector->expects($this->exactly(2))
+            ->method("camelize")
+            ->withConsecutive(
+                ["Test", Inflector::UPCASE_FIRST_LETTER],
+                ["Test", Inflector::DOWNCASE_FIRST_LETTER]
+            )->will($this->onConsecutiveCalls("Test", "test"));
+
+        $this->_inflector->expects($this->once())
+            ->method("underscore")
+            ->with("Test")
+            ->willReturn("test");
+
+        // Table name already set
+        $model->table = "PreSetTable";
         $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("PreSetTable", $model->table);
+
+        // Config denies table setting
+        $model->table = "";
+        $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("", $model->table);
+
+        // Table name set
+        $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("Test", $model->table);
+
+        // Table name pluralized
+        $model->table = "";
+        $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("Tests", $model->table);
+
+        // Table name camelized, ucfirst
+        $model->table = "";
+        $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("Test", $model->table);
+
+        // Table name camelized, lcfirst
+        $model->table = "";
+        $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("test", $model->table);
+
+        // Table name underscore
+        $model->table = "";
+        $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("test", $model->table);
+
+        // Table name uppercase 
+        $model->table = "";
+        $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("TEST", $model->table);
+
+        // Table name lowercase
+        $model->table = "";
+        $model->__construct($this->_logger, $this->_config, $this->_inflector, $this->_db);
+        $this->assertEquals("test", $model->table);
     }
 }
