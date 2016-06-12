2d124abff6449ffd5bb5168ab202de4c987d309f
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-06-12 11:58:43 +0200

    add error and no error exception classes

---
+++ src/Error.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Database Error
+ *
+ * Holds the information about a database error that occured while executing a query.
+ *
+ * @package   SlaxWeb\Database
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.4
+ */
+namespace SlaxWeb\Database;
+
+class Error
+{
+    use \SlaxWeb\GetSet\MagicGet;
+
+    /**
+     * Error message
+     *
+     * @var string
+     */
+    protected $_message = "";
+
+    /**
+     * Class constructor
+     *
+     * Sets the error data to protected properties.
+     *
+     * @param string $message Error message
+     * @return void
+     */
+    public function __construct(string $message)
+    {
+        $this->_message = $message;
+    }
+}
