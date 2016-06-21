066f5a4c0252486f5260afa2b02eb0a1666f33d8
Tomaz Lovrec <tomaz.lovrec@acl.at>
2016-06-21 09:18:30 +0200

    add the result object interface

---
+++ src/Interfaces/Result.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Database Result Interface
+ *
+ * The result interface provides the method signatures for each database library
+ * result class. Every result class muss implement this interface and provide the
+ * required functionality.
+ *
+ * @package   SlaxWeb\Database
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.4
+ */
+namespace SlaxWeb\Database\Interfaces;
+
+interface Result
+{
+    /**
+     * Class constructor
+     *
+     * Save the raw result data into the protected property '$_rawData' and parse
+     * it.
+     *
+     * @param array $rawData Raw data array
+     * @return void
+     */
+    public function __construct(array $data);
+
+    /**
+     * Next row
+     *
+     * Move the internal pointer to the next row of the result array. If there is
+     * no row found under the next index, bool(false) is returned, otherwise bool(true)
+     * is returned.
+     *
+     * @return bool
+     */
+    public function next(): bool;
+
+    /**
+     * Previous row
+     *
+     * Move the internal pointer to the previous row of the result array. If there
+     * is no row found under the previous index, bool(false) is returned, otherwise
+     * bool(true) is returned.
+     *
+     * @return bool
+     */
+    public function prev(): bool;
+
+    /**
+     * Jump to row
+     *
+     * Move the internal pointer to the passed in row of the result array. If there
+     * is no row found under the passed in row, bool(false) is returned, otherwise
+     * bool(true) is returned.
+     *
+     * @param int $row Row number
+     * @return bool
+     */
+    public function row(int $row): bool;
+}
