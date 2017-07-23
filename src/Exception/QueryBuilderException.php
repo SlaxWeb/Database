e0b08b6e449e3908f097e84c2b77ad045a2f42a8
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-07-23 13:36:05 +0200

    add the query builder exception

---
+++ src/Exception/QueryBuilderException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Query Builder Exception
+ *
+ * Thrown when the query builder encounters an unrecoverable error.
+ *
+ * @package   SlaxWeb\Database
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.6
+ */
+namespace SlaxWeb\Database\Exception;
+
+class QueryBuilderException extends \Exception
+{
+}
